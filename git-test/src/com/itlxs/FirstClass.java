b03ad1a46c0b1203a97c5d250e85a5c2439e5431
Tacit-lxs <2562174665@qq.com>
2020-12-02 08:50:14 +0800

    修改idea工程添加到仓库

--- git-test/src/com/itlxs/FirstClass.java
+++ git-test/src/com/itlxs/FirstClass.java
@@ -4,8 +4,12 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
         System.out.println("hello git project");
+        System.out.println("hshhss");
         System.out.println("hello git project");
         System.out.println("hello git project");
 
+    }
+    public void test(){
+        System.out.println("test");
     }
 }
